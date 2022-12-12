1b39d39959169f3575de046d6453b2c73d0fde92
juggernaut <sbagri.sumant@gmail.com>
2022-12-12 19:30:14 +0800

    Update config file (accommodate collision buffer)

--- src/gen_config.py
+++ src/gen_config.py
@@ -4,7 +4,7 @@ import glob
 import json
 import numpy as np
 
-from utils import load_image
+from fmt.utils import load_image
 
 MAPS_DIR = "../Maps/evaluation_data"
 CONFIG_DIR = "config"
@@ -13,22 +13,27 @@ RUNS_PER_MAP = 5
 
 config = {
     "max_num_steps": 2000, # maximum number of exploration steps for a single run before giving up
+    "runs_per_map": RUNS_PER_MAP, # number of runs for each map
+    "col_dst": 1.0, # min dist from obstacle for collision check
     "fmt": {
         "path_res": 0.1, # collision check for fmt
-        "col_dst": 1.0, # min dist from obstacle for fmt collision check
+        "heuristic_weights": [0.0, 1.0], # heuristic w eights for euclidean heuristics
         "batch_size": [10, 100, 200, 500, 1000, 2000, 5000, 10000],
+        "start_idx": 10000, # global starting index for fmt*
     },
     "bit": {
         "max_div": 50, # split for doing collision check (bit, nrrt)
-        "col_dst": 1.0, # min dist from obstacle for bit collision check
+        "start_idx": 20000, # global starting index for bit*
     },
     "nrrt": {
         "max_div": 200, # split for doing collision check (bit, nrrt)
-        "col_dst": 1.0, # min dist from obstacle for nrrt collision check
         "batch_size": [10, 100, 200, 500, 1000, 2000, 5000, 10000],
+        "start_idx": 30000, # global starting index for nrrt*
+        "steering_radius": 10 # steering radius for new node search
     },
     "maps": {}
 }
+
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
@@ -38,6 +43,19 @@ class NpEncoder(json.JSONEncoder):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
+
+def is_free(p, w):
+    pl = p[0] - int(4*config["col_dst"])
+    pr = p[0] + int(4*config["col_dst"])
+    pu = p[1] - int(4*config["col_dst"])
+    pd = p[1] + int(4*config["col_dst"])
+
+    c1 = pl > 0 and w[pl,p[1]] != 0
+    c2 = pr < w.shape[1]-1 and w[pr,p[1]] !=0
+    c3 = pu > 0 and w[p[0], pu] != 0
+    c4 = pd < w.shape[0]-1 and w[p[0], pd] != 0
+
+    return c1 and c2 and c3 and c4
 
 def main():
     for i, map in enumerate(sorted(glob.glob(f"{MAPS_DIR}/*"))):
@@ -51,9 +69,14 @@ def main():
         world = world[:,:,0]
         # Get the free world (pixels with value 255)
         free_world = np.argwhere(world == 255)
-        for run in range(RUNS_PER_MAP):
+        run = 0
+        while run < RUNS_PER_MAP:
             s, t = np.random.randint(free_world.shape[0], size=2)
-            config["maps"][f"map{i}"][f"s{run}"] = {"start": list(free_world[s]), "target": list(free_world[t])}
+            start = list(free_world[s])
+            target = list(free_world[t])
+            if is_free(start, world) and is_free(target, world):
+                config["maps"][f"map{i}"][f"s{run}"] = {"start": start, "target": target}
+                run += 1
 
     # Convert config dictionary to json file 
     with open(f"{CONFIG_DIR}/config.json", "w") as f:
